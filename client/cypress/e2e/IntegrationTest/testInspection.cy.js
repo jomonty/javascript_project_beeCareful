98ece08c3ecbb372bac9aac601168ad908f2e084
Maryhei <makemak123@gmail.com>
2023-01-25 11:52:16 +0000

    Trying to step in inspection tests

---
+++ client/cypress/e2e/IntegrationTest/testInspection.cy.js
@@ -0,0 +1,17 @@
+describe('Testing Inspections', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/colonies').wait(500)
+        cy.get("button").contains('See details').click({force: true}).wait(500)
+    })
+
+    it('should be able to see the inspections without pressing show button if have any', () => {
+        cy.get(':nth-child(1) > .card').should('not.be.visible')
+
+    })
+
+    it('should be able to see the inspections after pressing the show button in detail page if have any', () => {
+        cy.get('.Collapsible').click({force: true})
+        cy.get(':nth-child(1) > .card').should('be.visible')
+
+    })
+})
